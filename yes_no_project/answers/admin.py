c252f51eef400f59db94ee25dba8126dba96e857
mild <mild@PRONTOLAP0028.local>
2020-01-08 13:43:31 +0700

    [Update] admin.register in adminpy

--- yes_no_project/answers/admin.py
+++ yes_no_project/answers/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from answers.models import Answer
 
 # Register your models here.
-admin.site.register(Answer)
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id','text','image')
