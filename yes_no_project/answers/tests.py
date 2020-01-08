cb2f037a1202508ca0a5d4f4f72f560c8da07e0e
mild <mild@PRONTOLAP0028.local>
2020-01-08 17:07:25 +0700

    [Update] test CreateAnswerViewTest

--- yes_no_project/answers/tests.py
+++ yes_no_project/answers/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+# from django.urls import reverse
+from answers.models import Answer
 
-# Create your tests here.
+
+class CreateAnswerViewTest(TestCase):
+    def test_create_answer_shold_save_model(self):
+        response = self.client.post('/create_answer_class/', data={
+            'text': 'yes',
+            'image': 'https://media.giphy.com/media/ftqLysT45BJMagKFuk/giphy.gif'
+        })
+        self.assertEqual(response.status_code, 200)
+
+        ans = Answer.objects.last()
+        self.assertEqual(ans.text, 'yes')
+        self.assertEqual(
+            ans.image, 'https://media.giphy.com/media/ftqLysT45BJMagKFuk/giphy.gif')
